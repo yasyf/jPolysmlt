b4ec8559207ffac9c6eed6004018f231a3e44c7e
Yasyf Mohamedali <yasyf@yasyf.com>
2013-01-05 16:28:36 -0800

    added general query

--- src/polysmlt/Polysmlt.java
+++ src/polysmlt/Polysmlt.java
@@ -21,15 +21,21 @@ public class Polysmlt {
     private static String appid = "RUEHUK-7LYXQWXVWK";
 
     public static void main(String[] args) throws IOException, WAException {
+        String[] options = {"POLYSMLT", "Other"};
+        int option = JOptionPane.showOptionDialog(null, "Which Function?", "jMath", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        if (option == 0) {
 
-        degree = Integer.parseInt(JOptionPane.showInputDialog(null, "Degree", "2"));
-        int numb;
-        for (int i = degree; i >= 0; i--) {
-            numb = Integer.parseInt(JOptionPane.showInputDialog(null, "Coeficcient of X^" + i));
-            queryString += "+" + numb + "x^" + i;
+            degree = Integer.parseInt(JOptionPane.showInputDialog(null, "Degree", "2"));
+            int numb;
+            for (int i = degree; i >= 0; i--) {
+                numb = Integer.parseInt(JOptionPane.showInputDialog(null, "Coeficcient of X^" + i));
+                queryString += "+" + numb + "x^" + i;
+            }
+            queryString = "solve " + queryString + " over the reals";
+
+        } else {
+            queryString = JOptionPane.showInputDialog(null, "Enter Query");
         }
-        queryString = "roots of " + queryString;
-        System.out.println(queryString);
         WAEngine engine = new WAEngine();
         engine.setAppID(appid);
         engine.addFormat("plaintext");
@@ -39,16 +45,32 @@ public class Polysmlt {
         WAQueryResult queryResult = engine.performQuery(query);
         String result = "";
         Integer resultNumb = 0;
-        for (WAPod pod : queryResult.getPods()) {
-            if ("RootsInTheComplexPlane".equals(pod.getID())) {
-                break;
-            }
-            for (WASubpod subpod : pod.getSubpods()) {
-                for (Object element : subpod.getContents()) {
-                    if (element instanceof WAPlainText) {
-                        resultNumb++;
-                        if (resultNumb > 1 && !"".equals(((WAPlainText) element).getText())) {
-                            result += ((WAPlainText) element).getText() + "\n\n";
+        String temp;
+        if (queryResult.isError()) {
+            result = queryResult.getErrorMessage();
+        } else {
+            fetch:
+            {
+                for (WAPod pod : queryResult.getPods()) {
+                    if ("RootsInTheComplexPlane".equals(pod.getID())) {
+                        break fetch;
+                    } else {
+                        for (WASubpod subpod : pod.getSubpods()) {
+                            for (Object element : subpod.getContents()) {
+                                if (element instanceof WAPlainText) {
+                                    resultNumb++;
+                                    temp = ((WAPlainText) element).getText();
+                                    if (resultNumb > 1 && !"".equals(temp)) {
+                                        if (resultNumb > 2) {
+                                            result += "\n\n";
+                                        }
+                                        result += temp;
+                                        if ("(no real solutions)".equals(temp)) {
+                                            break fetch;
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
                 }
